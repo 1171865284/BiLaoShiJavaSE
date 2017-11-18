d1bae444436f86c2b62c7bb121531e8072b18a04
江南 <@DESKTOP-DF09JQM>
2017-11-18 12:32:58 +0800

    线程的使用

---
+++ src/day13/TicketDemo.java
@@ -0,0 +1,63 @@
+package day13;
+
+/*
+ * 程序功能:卖票实例
+ * 程序员:魏国平
+ * 编写时间:11月17日
+ */
+
+/*
+ * 需求：卖票
+ */
+
+class Ticket implements Runnable //extends Thread
+{
+	private int num = 100;
+	
+	public void run()
+	{
+		sale();
+	}
+	
+	public void sale()
+	{
+		while(true){
+			//因为在现实中的售票是没有出现负票的情况的
+			if(num > 0)
+			{
+				System.out.println(Thread.currentThread().getName() + ".....sale...." + num--);
+			}
+		}
+	}
+}
+
+public class TicketDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Ticket t1 = new Ticket();
+		Ticket t2 = new Ticket();
+		Ticket t3 = new Ticket();
+		Ticket t4 = new Ticket();
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();*/
+		
+		Ticket t = new Ticket();//创建一个线程任务对象(这个卖火车票的)
+		Ticket tt = new Ticket();//创建一个线程任务对象(这个卖动车票的)
+		
+		
+		Thread t1 = new Thread(t);
+		Thread t2 = new Thread(t);
+		Thread t3 = new Thread(tt);
+		Thread t4 = new Thread(tt);
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+	}
+
+}
